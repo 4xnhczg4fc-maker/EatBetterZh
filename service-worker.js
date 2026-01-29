4adb58b09e2a772904b34ad774395510ddb7ca66
4xnhczg4fc-maker <4xnhczg4fc@privaterelay.appleid.com>
2026-01-29 10:09:40 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,5 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("food-finder-v1").then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./restaurants.json"
-      ])
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+self.addEventListener("install", e=>{
+ e.waitUntil(
+  caches.open("foodfinder").then(c=>c.addAll(["./"]))
+ );
 });
